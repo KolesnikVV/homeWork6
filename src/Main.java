b092212b5d42869fb584f785b2b82bd682199383
KolesnikVV <Voltpluc@ya.ru>
2023-02-15 17:08:44 +0300

    tas1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+        int clientOS = 0;
+        if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");}
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для Android по ссылке");}
     }
 }
